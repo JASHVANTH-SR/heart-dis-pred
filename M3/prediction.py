856501f3672eaac49b129359fb9cabd5ee9ba98f
Harul <83761023+harul9503@users.noreply.github.com>
2023-04-29 12:14:43 +0530

    Update prediction.py

--- M3/prediction.py
+++ M3/prediction.py
@@ -30,7 +30,7 @@ column_names = [
             "Oldpeak - (1 to 3):",  #10
             "Slope - (1, 2, 3):",  #11
             "CA - (0 to 3):", #12
-            "Thal - (3, 6, 7):", #13
+            "Thal - (0, 1):", #13
             "target"  #14
         ]
 
